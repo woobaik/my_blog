8ef09e93069d8ff84242020bfbba78911c6e183f
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-07-20 00:38:10 -0700

    Create seeds file for sample data

--- Portfolio/db/seeds.rb
+++ Portfolio/db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "My Blog Post #{blog}",
+    body: "Computer Sience start from 1"
+
+  )
+end
+
+puts "10 BLOG POST CREATED"
+
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill}",
+    percent_utilized: skill
+  )
+end
+
+puts "5 SKILLS CREATED!"
+
+9.times do |port|
+  Portfol.create!(
+    title: "Portfolio Title #{port}",
+    subtitle: "My Great Service",
+    body: "Ipsum Lorem haram consectetur",
+    main_image: "http://via.placeholder.com/600x400",
+    thumb_image: "http://via.placeholder.com/350x250"
+  )
+end
+puts "9 Portfolio items created"
