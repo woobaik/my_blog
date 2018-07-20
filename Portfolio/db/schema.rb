750bb2766a7317802298ce4c0e32aecb201d58ca
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-07-19 23:58:26 -0700

    Create Portfol via resource generator

--- Portfolio/db/schema.rb
+++ Portfolio/db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_20_060003) do
+ActiveRecord::Schema.define(version: 2018_07_20_065439) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "portfols", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.text "body"
+    t.text "main_image"
+    t.text "thumb_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
