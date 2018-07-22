4efd51abcdccda0508a06d83c7635dd8683a2846
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-07-21 22:57:42 -0700

    Create portfol_params for white listing

--- Portfolio/app/controllers/portfols_controller.rb
+++ Portfolio/app/controllers/portfols_controller.rb
@@ -3,4 +3,9 @@ class PortfolsController < ApplicationController
   def index
     @portfols = Portfol.all
   end
+
+  private
+  def portfol_params
+    params.require(:portfol).permit(:title, :subtitle, :body, :main_image, :thumb_image)
+  end
 end
