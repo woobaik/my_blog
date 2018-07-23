4b0c0e87e04562e92ab36dc2a32cf485b072787a
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-07-22 20:23:56 -0700

    Create new and create form for portfols

--- Portfolio/app/controllers/portfols_controller.rb
+++ Portfolio/app/controllers/portfols_controller.rb
@@ -4,6 +4,22 @@ class PortfolsController < ApplicationController
     @portfols = Portfol.all
   end
 
+  def new
+    @portfol = Portfol.new
+  end
+
+  def create
+    @portfol = Portfol.new(portfol_params)
+
+    respond_to do |format|
+      if @portfol.save
+        format.html { redirect_to portfols_path, notice: 'Your Portfolio item is now live'}
+      else
+        format.html {render :new}
+      end
+    end
+  end
+
   private
   def portfol_params
     params.require(:portfol).permit(:title, :subtitle, :body, :main_image, :thumb_image)
