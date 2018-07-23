6529ca7e5aa40b9551ef26c34517d7c55aa74231
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-07-23 02:57:06 -0700

    Create enum for blog post

--- Portfolio/app/models/blog.rb
+++ Portfolio/app/models/blog.rb
@@ -1,4 +1,6 @@
 class Blog < ApplicationRecord
+  enum post_status: { draft: 0, published: 1}
   extend FriendlyId
   friendly_id :title, use: :slugged
+
 end
--- Portfolio/db/schema.rb
+++ Portfolio/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_23_090208) do
+ActiveRecord::Schema.define(version: 2018_07_23_095206) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_07_23_090208) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.integer "post_status", default: 0
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
