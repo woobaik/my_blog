8f08461391e48a470e28e3b70e4a165a476e114e
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-07-24 23:38:35 -0700

    Add validation for Portfol

--- Portfolio/app/models/portfol.rb
+++ Portfolio/app/models/portfol.rb
@@ -1,2 +1,6 @@
 class Portfol < ApplicationRecord
+  validates :title, presence: true
+  validates :subtitle, presence: true
+  validates :main_image, presence: true
+  validates :thumb_image, presence: true
 end
