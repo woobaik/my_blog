64a5d216b0f5405f813f9b2b5d770268f3689877
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-07-25 22:49:37 -0700

    Migrate topic blogs association

--- Portfolio/db/schema.rb
+++ Portfolio/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_25_065316) do
+ActiveRecord::Schema.define(version: 2018_07_25_065811) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2018_07_25_065316) do
     t.datetime "updated_at", null: false
     t.string "slug"
     t.integer "post_status", default: 0
+    t.integer "topic_id"
+    t.index ["topic_id"], name: "index_blogs_on_topic_id"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
