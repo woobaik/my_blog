acf5a93fc6a00aca9efda8d51544bdbcfb796ef3
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-08-21 18:36:01 -0700

    Create copyright module

--- Portfolio/app/controllers/application_controller.rb
+++ Portfolio/app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_copyright
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:signup, keys: [:name])
@@ -10,5 +11,16 @@ class ApplicationController < ActionController::Base
   include CurrentUserConcern
   include DefaultPageContent
 
+  def set_copyright
+    @copyright = JoungwooViewTool::Renderer.copyright("Joungwoo Baik", "All rights revserved")
+  end
 
+end
+
+module JoungwooViewTool
+  class Renderer
+    def self.copyright(name, msg)
+      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+    end
+  end
 end
